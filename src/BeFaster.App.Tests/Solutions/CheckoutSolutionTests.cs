b104cf8fe3063d0ecc875fbcb15541ecc3de58ba
sbx_user1059 <sbx_user1059@ip-10-12-114-243.eu-west-2.compute.internal>
2018-04-16 13:28:38 +0000

    Mon Apr 16 13:28:38 UTC 2018

--- src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
@@ -24,6 +24,22 @@ namespace BeFaster.App.Tests.Solutions
         [TestCase("HHHHHHHHHH", ExpectedResult = 80)]
         [TestCase("HHHHHHHHHHHHHHH", ExpectedResult = 125)]
         [TestCase("HHHHHHHHHHHHHHHH", ExpectedResult = 135)]
+        [TestCase("K", ExpectedResult = 80)]
+        [TestCase("KK", ExpectedResult = 150)]
+        [TestCase("NNN", ExpectedResult = 120)]
+        [TestCase("NNMN", ExpectedResult = 120)]
+        [TestCase("PPPP", ExpectedResult = 200)]
+        [TestCase("PPPPP", ExpectedResult = 200)]
+        [TestCase("QQ", ExpectedResult = 60)]
+        [TestCase("QQQ", ExpectedResult = 80)]
+        [TestCase("RRR", ExpectedResult = 150)]
+        [TestCase("RRRQ", ExpectedResult = 150)]
+        [TestCase("RRRQRRQQ", ExpectedResult = 310)]
+        [TestCase("UUU", ExpectedResult = 120)]
+        [TestCase("UUUU", ExpectedResult = 120)]
+        [TestCase("V", ExpectedResult = 50)]
+        [TestCase("VV", ExpectedResult = 90)]
+        [TestCase("VVV", ExpectedResult = 130)]
         public int ComputeCheckoutTotal(string skus)
         {
             return CheckoutSolution.Checkout(skus);
