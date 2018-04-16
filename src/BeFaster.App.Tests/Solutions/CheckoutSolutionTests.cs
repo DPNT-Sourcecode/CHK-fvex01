5bbcc6b86d15daf8b00432ed22ebdd7889b07997
sbx_user1059 <sbx_user1059@ip-10-12-114-243.eu-west-2.compute.internal>
2018-04-16 11:37:07 +0000

    Mon Apr 16 11:37:07 UTC 2018

---
+++ src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using BeFaster.App.Solutions;
+
+namespace BeFaster.App.Tests.Solutions
+{
+    [TestFixture]
+    public class CheckoutSolutionTests
+    {
+        [TestCase("John", ExpectedResult = "Hello, John!")]
+        [TestCase("Mary", ExpectedResult = "Hello, Mary!")]
+        [TestCase(" ", ExpectedResult = "Hello,  !")]
+        public int ComputeSum(string skus)
+        {
+            return CheckoutSolution.Checkout(skus);
+        }
+    }
+}
