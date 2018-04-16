8f05b664dcda18da3ef5e811af58f6e67572cd2b
sbx_user1059 <sbx_user1059@ip-10-12-114-243.eu-west-2.compute.internal>
2018-04-16 11:52:07 +0000

    Mon Apr 16 11:52:07 UTC 2018

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -1,4 +1,6 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BeFaster.App.Solutions
 {
@@ -6,7 +8,22 @@ namespace BeFaster.App.Solutions
     {
         public static int Checkout(string skus)
         {
-            throw new SolutionNotImplementedException();
+            var chars = skus.ToCharArray().GroupBy(c => c);
+
+            var priceDictionary = GetPrices();
+
+        }
+
+        private static object GetPrices()
+        {
+            var prices = new Dictionary<char, int>();
+
+            prices.Add('A', 50);
+            prices.Add('B', 30);
+            prices.Add('C', 20);
+            prices.Add('D', 15);
+
+            return prices;
         }
     }
 }
