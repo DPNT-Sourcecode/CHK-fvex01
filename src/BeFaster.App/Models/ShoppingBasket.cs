4a5f6289edd79cf44f03e7cb262677b87b39e040
sbx_user1059 <sbx_user1059@ip-10-12-114-243.eu-west-2.compute.internal>
2018-04-16 13:04:38 +0000

    Mon Apr 16 13:04:38 UTC 2018

--- src/BeFaster.App/Models/ShoppingBasket.cs
+++ src/BeFaster.App/Models/ShoppingBasket.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BeFaster.App.Models
 {
-    class ShoppingBasket
+    public class ShoppingBasket
     {
+        private static List<char> _allowedCharacters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F' };
+
+        public ShoppingBasket(string skus)
+        {
+            Orders = skus
+                .ToCharArray()
+                .GroupBy(c => c, (key, g) => new ItemOrder() { Sku = key, Count = g.Count() })
+                .ToList();
+        }
+
+        public int CalculateTotalPrice()
+        {
+            var numberOfEItems = basket.FirstOrDefault(g => g.Sku.Equals('E'))?.Count ?? 0;
+            if (numberOfEItems > 0)
+            {
+                var numberOfFreeBItems = numberOfEItems / 2;
+                var currentBCount = basket.FirstOrDefault(g => g.Sku.Equals('B'))?.Count ?? 0;
+                if (currentBCount > 0)
+                {
+                    var DiscountedBItems = currentBCount - numberOfFreeBItems;
+                    basket.FirstOrDefault(g => g.Sku.Equals('B')).Count = Math.Max(0, DiscountedBItems);
+                }
+            }
+
+            var total = 0;
+            foreach (var group in basket)
+            {
+                total += SumItemPrices(group.Sku, group.Count);
+            }
+
+            return total;
+        }
+
+        public List<ItemOrder> Orders { get; set; }
+
+        public bool IsValid => Orders.All(c => _allowedCharacters.Contains(c.Sku))
     }
 }
--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -7,39 +7,19 @@ namespace BeFaster.App.Solutions
 {
     public static class CheckoutSolution
     {
-        private static List<char> _allowedCharacters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F' };
         
         public static int Checkout(string skus)
         {
-            var basket = skus
-                .ToCharArray()
-                .GroupBy(c => c, (key, g) => new ItemOrder() { Sku = key, Count = g.Count() })
-                .ToList();
+            var basket = new ShoppingBasket(skus);
 
-            if(basket.Any(c => !_allowedCharacters.Contains(c.Sku)))
+            if(!basket.IsValid)
             {
                 return -1;
             }
 
-            var numberOfEItems = basket.FirstOrDefault(g => g.Sku.Equals('E'))?.Count ?? 0;
-            if(numberOfEItems > 0)
-            {
-                var numberOfFreeBItems = numberOfEItems / 2;
-                var currentBCount = basket.FirstOrDefault(g => g.Sku.Equals('B'))?.Count ?? 0;
-                if(currentBCount > 0)
-                {
-                    var DiscountedBItems = currentBCount - numberOfFreeBItems;
-                    basket.FirstOrDefault(g => g.Sku.Equals('B')).Count = Math.Max(0, DiscountedBItems);
-                }
-            }
+            return basket.CalculateTotalPrice();
 
-            var total = 0;
-            foreach (var group in basket)
-            {
-                total += SumItemPrices(group.Sku, group.Count);
-            }
-
-            return total;
+            
         }
 
         private static int SumItemPrices(char sku, int count)
