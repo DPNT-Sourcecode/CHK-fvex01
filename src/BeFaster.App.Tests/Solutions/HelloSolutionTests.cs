3ee1763c2d73267e226f195511abd541d12b9b66
sbx_user1059 <sbx_user1059@ip-10-12-114-243.eu-west-2.compute.internal>
2018-04-16 11:33:49 +0000

    Mon Apr 16 11:33:49 UTC 2018

--- src/BeFaster.App.Tests/Solutions/HelloSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/HelloSolutionTests.cs
@@ -6,8 +6,10 @@ namespace BeFaster.App.Tests.Solutions
     [TestFixture]
         public class HelloSolutionTests
         {
-            [TestCase("hey", ExpectedResult = "Hello, World!")]
-            public string ComputeSum(string x)
+            [TestCase("John", ExpectedResult = "Hello, John!")]
+            [TestCase("Mary", ExpectedResult = "Hello, Mary!")]
+            [TestCase(" ", ExpectedResult = "Hello,  !")]
+        public string ComputeSum(string x)
             {
                 return HelloSolution.Hello(x);
             }
